eb367d67942f43576e16d75901e176a809e81472
Daniel Owens <owensdaniel8@yahoo.com>
2022-01-26 03:08:23 -0800

    complete first project

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Follow along with the examples here
+function say(names, footballTeams) {
+    console.log("Hello, Mate");
+    return `${names}, ${footballTeams}!`;
+}
+console.log(say("Daniel", "Vikings"))
